4a91b358d5725a7835ca7e4daed4d1168003e342
Mike <bigapewhat@gmail.com>
2018-01-26 15:21:42 -0700

    transaction update

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var port = process.env.PORT || 1347;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', function (req, res) { res.status(200).send('Hello world!'); });
+
 app.listen(port, function () {
   console.log('Listening on port ' + port);
 });
