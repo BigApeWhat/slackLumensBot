26ed66f18cd1cd33e01c01c08c28c19e6b365ff1
Mike <bigapewhat@gmail.com>
2018-01-26 19:08:59 -0700

    updated tranaction endpoint

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.post('/transactions', function (req, res, next) {
           });
           response.on('end', function() {
               var parsed = JSON.parse(body);
-              console.log(body);
               var botPayload = {
                 email: parsed._links,
                 password: parsed.self
@@ -32,3 +31,29 @@ app.post('/transactions', function (req, res, next) {
           });
       });
 });
+
+app.get('/account', function (req, res, next) {
+  var request = https.get({
+          host: hostUrl,
+          path: `/accounts/${req.body.text}`
+      }, function(response) {
+          var body = '';
+          response.on('data', function(d) {
+              body += d;
+          });
+          response.on('end', function() {
+              var parsed = JSON.parse(body);
+
+              var total = 0
+              parsed.balances.forEach(function(entry) {
+                total += parseFloat(entry.balance)
+              });
+
+              var botPayload = {
+                    text : 'Your account balance is ' + total + ' lumens'
+              };
+              return res.status(200).json(botPayload);
+          });
+      });
+});
+//https://horizon.stellar.org/accounts/GDG2NE5JOLF5GHTEWLMS2N7SW3LFLAZ7HYY7JMADS33ZGC5UDLXC2WLE/transactions
