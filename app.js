4cd2c0adb2605fbbccad2edeca4ffdc2074b218b
Mike <bigapewhat@gmail.com>
2018-01-27 15:03:21 -0700

    updated managers;

--- app.js
+++ app.js
@@ -11,6 +11,28 @@ var hostUrl = 'horizon.stellar.org'
 var marketCapUrl = 'api.coinmarketcap.com'
 // var hostUrl ='horizon-testnet.stellar.org'
 
+setInterval(function() {
+  var request = https.get({
+          host: marketCapUrl,
+          path: `/v1/ticker/`
+      }, function(response) {
+          var body = '';
+          response.on('data', function(d) {
+              body += d;
+          });
+          response.on('end', function() {
+              var parsed = JSON.parse(body);
+              var value = valueManager.getId(parsed, 'XLM');
+
+              var botPayload = {
+                    text : value.price_usd + " usd"
+              };
+              console.log(botPayload);
+              //return res.status(200).json(botPayload);
+          });
+      });
+    }, 5000);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, function () {
@@ -56,24 +78,7 @@ app.post('/account', function (req, res, next) {
 });
 
 app.get('/value', function (req, res, next) {
-  var request = https.get({
-          host: marketCapUrl,
-          path: `/v1/ticker/`
-      }, function(response) {
-          var body = '';
-          response.on('data', function(d) {
-              body += d;
-          });
-          response.on('end', function() {
-              var parsed = JSON.parse(body);
-              var value = valueManager.getId(parsed, 'XLM');
 
-              var botPayload = {
-                    text : value.price_usd + " usd"
-              };
-              return res.status(200).json(botPayload);
-          });
-      });
 });
 // lumen value
 //https://horizon.stellar.org/accounts/GDG2NE5JOLF5GHTEWLMS2N7SW3LFLAZ7HYY7JMADS33ZGC5UDLXC2WLE/transactions
