9546dcd93d205353640180afa5074f3b0c950fc2
Mike <bigapewhat@gmail.com>
2018-01-26 22:45:31 -0700

    updated tranaction endpoint

--- app.js
+++ app.js
@@ -62,9 +62,9 @@ app.post('/account', function (req, res, next) {
                 var value = parseFloat(entry.balance)
                 if (value != 0) {
                   if (entry.asset_type == 'native') {
-                    accountText += value + ' lumen\n'
+                    accountText += value + ' Lumen\n'
                   } else {
-                    accountText += value + entry.asset_code + '\n'
+                    accountText += value + ' ' + entry.asset_code + '\n'
                   }
                 }
               });
