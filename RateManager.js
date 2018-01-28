b8750a6711751cae65892a125d37248e254d91a1
Mike <bigapewhat@gmail.com>
2018-01-27 18:41:33 -0700

    updated

--- RateManager.js
+++ RateManager.js
@@ -7,7 +7,7 @@ module.exports = {
     if (value == null) {
       return 'Invalid currency selected.'
     } else if (amount != null){
-      return amount + ' XLM is worth ' + amount * parseFloat(value).toFixed(2) + ' ' + rate.toUpperCase()
+      return amount + ' XLM is worth ' + (amount * parseFloat(value)).toFixed(2) + ' ' + rate.toUpperCase()
     } else {
       return 'XLM is worth ' + parseFloat(value).toFixed(2) + ' ' + rate.toUpperCase()
     }
