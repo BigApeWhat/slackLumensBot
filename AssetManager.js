577e04252174aaef58451c897dd386a647f27475
Mike <bigapewhat@gmail.com>
2018-01-31 20:19:00 -0700

    updates

--- AssetManager.js
+++ AssetManager.js
@@ -1,10 +1,10 @@
 module.exports = {
   getAssets: function(records) {
-    let exitText = 'Assets currently available\n\n'
+    let exitText = '~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
     records.forEach(function(entry, i) {
       exitText += 'Asset type: ' + entry.asset_type + '\nAsset code: ' + entry.asset_code +
       '\nAsset issuer: ' + entry.asset_issuer + '\nPaging token: ' + entry.paging_token + '\nAmount: ' +
-      entry.amount + '\nNumber accounts: ' + entry.accounts + '\n\nFlags\n    Auth required: ' + entry.flags.auth_required +
+      entry.amount + '\nNumber accounts: ' + entry.num_accounts + '\n\nFlags\n    Auth required: ' + entry.flags.auth_required +
       '\n    Auth revocable: ' + entry.flags.auth_revocable + '\n~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n'
     });
     return exitText
